3aba14a1049508bb9755652ac0626612a7547033
Sindre Røkenes Myren <sindre@searis.no>
2017-06-14 13:26:47 +0200

    Extended timestamps to support year 1601
    Due to int64 overflows, the original Marshal/Unmarshal for time
    stamps did not support times earlier than 1677-09-21 00:12:43.145224192
    +0000 UTC. This change supports earlier time stamps which is useful
    because the Windows NT Zero time is January 1, 1601 00:00:00.0.
    
    Being able to correctly encode the Zero time, makes tests easier to
    write, and may help debugging when incorrect timestamps are recieved
    from an OPC Server with an invalid system time.

--- uatype/encoding/binary/date_time.go
+++ uatype/encoding/binary/date_time.go
@@ -1,12 +1,17 @@
 package binary
 
 import "time"
-import "bytes"
+
 import "encoding/binary"
 
-// secondsToUnixEpoch counts the number of seconds since Windows NT time epoch
-// (January 1 1601) to the Unix/POSIX time epoch.
-const secondsToUnixEpoch int64 = 11644473600
+// secondsToUnixEpoch counts the number of seconds since the Windows NT time
+// epoch (January 1 1601) to the Unix/POSIX time epoch.
+const (
+	secondsToUnixEpoch            int64 = 11644473600
+	hundredNanoSecondsToUnixEpoch int64 = secondsToUnixEpoch * 1e7
+)
+
+const minInt64 int64 = -1 << 63
 
 // dateTime converts between OPC UA DateTime 64-bit binary representation
 // (Windwows NT timestamp) and Go time.Time structs.
@@ -15,31 +20,27 @@ type dateTime time.Time
 // MarshalBinary encodes a time.Time struct into a 64-bit OPC UA DateTime
 // representation.
 func (t dateTime) MarshalBinary() ([]byte, error) {
-	var buf bytes.Buffer
+	b := make([]byte, 8)
 
-	// nanoseconds (e-9) -> milliseconds (e-7).
-	i := time.Time(t).UnixNano() / 100
-	i += secondsToUnixEpoch * int64(1e7)
+	sec := time.Time(t).Unix()
+	nsec := time.Time(t).Nanosecond()
 
-	binary.Write(&buf, binary.LittleEndian, i)
-	return buf.Bytes(), nil
+	i := sec*1e7 + hundredNanoSecondsToUnixEpoch + int64(nsec/100)
+	binary.LittleEndian.PutUint64(b, uint64(i))
+
+	return b, nil
 }
 
 // UnmarshalBinary decodes a 64-bit Windows NT timestamp into a Go time.Time
 // struct.
 func (t *dateTime) UnmarshalBinary(data []byte) error {
-	var i int64
+	i := int64(binary.LittleEndian.Uint64(data[0:8]))
+	i -= hundredNanoSecondsToUnixEpoch
 
-	buf := bytes.NewBuffer(data[0:8])
-	if err := binary.Read(buf, binary.LittleEndian, &i); err != nil {
-		return err
-	}
-	// NB! secondsToUnixEpoch * int64(1e9) would overflow.
-	i -= secondsToUnixEpoch * int64(1e7)
-	// milliseconds (e-7) -> nanoseconds (e-9).
-	nsec := i * 100
+	sec := i / 1e7
+	nsec := (i - sec*1e7) * 100
 
-	*t = dateTime(time.Unix(0, nsec).UTC())
+	*t = dateTime(time.Unix(sec, nsec).UTC())
 	return nil
 }
 
