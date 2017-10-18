37839a3f082de4ae10d6d8ef0cd24a84cd540abb
Tor-Inge J. Eriksen <tor@searis.no>
2017-10-18 13:46:03 +0200

    Bug-fix: Wrong path for uatype in generate script

--- stack/uatype/error.go
+++ stack/uatype/error.go
@@ -1,4 +1,4 @@
-//go:generate csv2code -o error_auto.go  -csv ../schemas/1.03/Opc.Ua.StatusCodes.csv errors_auto.go.tmpl
+//go:generate csv2code -o error_auto.go  -csv ../../schemas/1.03/Opc.Ua.StatusCodes.csv errors_auto.go.tmpl
 //go:generate gofmt -s -w error_auto.go
 
 package uatype
